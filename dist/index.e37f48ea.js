ebae59269fe2d194728eb3c5eb174fd925ed8063
Jasurbek <jasurisoqov685@gmail.com>
2022-04-12 17:23:42 +0500

    render Html

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -546,7 +546,102 @@ const showRecipe = async function() {
         time: obj.cooking_time
     };
 };
-showRecipe(); // O'ng tomondagi malumotlarni render qilib chiqaruvch function
+showRecipe();
+// ingredientlarni render qilib asosiy render functionga beruvchi function
+const renderIng = function(ings) {
+    return ings.map((val)=>{
+        return `<li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="${_iconsSvgDefault.default}#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${val.quantity ? val.quantity : ''}</div>
+    <div class="recipe__description">
+      <span class="recipe__unit">${val.unit}</span>
+      ${val.description}
+    </div>
+  </li>
+`;
+    });
+};
+// O'ng tomondagi malumotlarni render qilib chiqaruvch function
+const renderHtml = function(data) {
+    const html = `<figure class="recipe__fig">
+  <img src="${data / image}" alt="${data.title}" class="recipe__img" />
+  <h1 class="recipe__title">
+    <span>${data.title}</span>
+  </h1>
+</figure>
+
+<div class="recipe__details">
+  <div class="recipe__info">
+    <svg class="recipe__info-icon">
+      <use href="${_iconsSvgDefault.default}#icon-clock"></use>
+    </svg>
+    <span class="recipe__info-data recipe__info-data--minutes">${data.time}</span>
+    <span class="recipe__info-text">minutes</span>
+  </div>
+  <div class="recipe__info">
+    <svg class="recipe__info-icon">
+      <use href="${_iconsSvgDefault.default}#icon-users"></use>
+    </svg>
+    <span class="recipe__info-data recipe__info-data--people">${data.servings}</span>
+    <span class="recipe__info-text">servings</span>
+
+    <div class="recipe__info-buttons">
+      <button class="btn--tiny btn--increase-servings">
+        <svg>
+          <use href="${_iconsSvgDefault.default}#icon-minus-circle"></use>
+        </svg>
+      </button>
+      <button class="btn--tiny btn--increase-servings">
+        <svg>
+          <use href="${_iconsSvgDefault.default}#icon-plus-circle"></use>
+        </svg>
+      </button>
+    </div>
+  </div>
+
+  <div class="recipe__user-generated">
+    <svg>
+      <use href="${_iconsSvgDefault.default}#icon-user"></use>
+    </svg>
+  </div>
+  <button class="btn--round">
+    <svg class="">
+      <use href="${_iconsSvgDefault.default}#icon-bookmark-fill"></use>
+    </svg>
+  </button>
+</div>
+
+<div class="recipe__ingredients">
+  <h2 class="heading--2">Recipe ingredients</h2>
+  <ul class="recipe__ingredient-list">
+
+  ${renderIng(data.ingredients).join('')}
+
+  </ul>
+</div>
+
+<div class="recipe__directions">
+  <h2 class="heading--2">How to cook it</h2>
+  <p class="recipe__directions-text">
+    This recipe was carefully designed and tested by
+    <span class="recipe__publisher">${data.publisher}</span>. Please check out
+    directions at their website.
+  </p>
+  <a
+    class="btn--small recipe__btn"
+    href="${data.url}"
+    target="_blank"
+  >
+    <span>Directions</span>
+    <svg class="search__icon">
+      <use href="${_iconsSvgDefault.default}#icon-arrow-right"></use>
+    </svg>
+  </a>
+</div>`;
+    recipeContainer.insertAdjacentHTML('afterbegin', html);
+};
 
 },{"regenerator-runtime":"dXNgZ","../img/icons.svg":"cMpiy","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"dXNgZ":[function(require,module,exports) {
 /**
