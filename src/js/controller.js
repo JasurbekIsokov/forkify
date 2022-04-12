500050a613f654a77162226a3b14c9c58885dafb
Jasurbek <jasurisoqov685@gmail.com>
2022-04-12 17:29:00 +0500

    window location hash pustoy id

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,23 +19,29 @@ const timeout = function (s) {
 
 // Bitta taom retseptini oluvchi funcsiya
 const showRecipe = async function () {
-  const data = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
-  );
-  const dataJSON = await data.json();
-  const obj = dataJSON.data.recipe;
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    const data = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const dataJSON = await data.json();
+    const obj = dataJSON.data.recipe;
 
-  // Malumotlarni saqlovchi obect.
-  const state = {
-    id: obj.id,
-    image: obj.image_url,
-    publisher: obj.publisher,
-    ingredients: obj.ingredients,
-    title: obj.title,
-    servings: obj.servings,
-    url: obj.source_url,
-    time: obj.cooking_time,
-  };
+    // Malumotlarni saqlovchi obect.
+    const state = {
+      id: obj.id,
+      image: obj.image_url,
+      publisher: obj.publisher,
+      ingredients: obj.ingredients,
+      title: obj.title,
+      servings: obj.servings,
+      url: obj.source_url,
+      time: obj.cooking_time,
+    };
+  } catch (Error) {
+    alert(Error);
+  }
 };
 
 showRecipe();
